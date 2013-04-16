446df174d32b892ec42942fbaae2d69488fcca3e
Thomas Aylott <oblivious@subtlegradient.com>
2013-04-16 16:31:16 -0700

    psEval should support functions & args like invoke

--- test/test-photoshop-eval.js
+++ test/test-photoshop-eval.js
@@ -1,6 +1,27 @@
 var t = require('tap')
 var psEval = require('../lib/photoshop-eval')
 
+t.test("number returns the correct result", function(t){
+  t.plan(1)
+  psEval(123).on('data', function(data){
+    t.is(JSON.parse(data), 123)
+  })
+})
+
+t.test("function returns the correct result", function(t){
+  t.plan(1)
+  psEval(function(){return 1 + 1}).on('data', function(data){
+    t.is(JSON.parse(data), 2)
+  })
+})
+
+t.test("function with args returns the correct result", function(t){
+  t.plan(1)
+  psEval(function(a, b){return a + b}, [1, 2]).on('data', function(data){
+    t.is(JSON.parse(data), 3)
+  })
+})
+
 t.test("script return the correct result", function(t){
   t.plan(1)
   psEval('1 + 1').on('data', function(data){
