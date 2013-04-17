0c42769f1a85221ce88c13c4e14ae00dfb15b3a1
Thomas Aylott <oblivious@subtlegradient.com>
2013-04-16 17:47:09 -0700

    better args handling

--- test/test-stream-basic.js
+++ test/test-stream-basic.js
@@ -64,3 +64,53 @@ t.test('stream write single dual', function(t){
   
   stream.on('end', t.end.bind(t))
 })
+
+t.test('stream write multiple args', function(t){
+  
+  var createScraper = require('json-scrape')
+  
+  var stream = createScraper()
+  
+  psStream(function(stream, a, b){
+    // var stream = arguments[arguments.length-1]
+    stream.write('{"lulz":' + a(10) + '}')
+    stream.write('{"lulz":' + b + '}')
+  }, [function(c){return 12.3 * c}, 456]).pipe(stream)
+  
+  t.plan(4)
+  
+  stream.once('data', function(object){
+    t.is(typeof object, 'object')
+    t.is(object.lulz, 123)
+    stream.once('data', function(object){
+      t.is(typeof object, 'object')
+      t.is(object.lulz, 456)
+    })
+  })
+  
+  stream.on('end', t.end.bind(t))
+})
+
+t.test('stream write single dual', function(t){
+  
+  var createScraper = require('json-scrape')
+  
+  var stream = createScraper()
+  
+  psStream(function(stream){
+    stream.write('{"lulz":true}{"lulz":false}')
+  }).pipe(stream)
+  
+  t.plan(4)
+  
+  stream.once('data', function(object){
+    t.is(typeof object, 'object')
+    t.is(object.lulz, true)
+    stream.once('data', function(object){
+      t.is(typeof object, 'object')
+      t.is(object.lulz, false)
+    })
+  })
+  
+  stream.on('end', t.end.bind(t))
+})
