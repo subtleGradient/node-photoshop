a7431cbeefff6cdd86d7b51e8174fe046df2589c
Thomas Aylott <oblivious@subtlegradient.com>
2013-04-19 15:56:02 -0700

    layer stream test

---
+++ test/test-stream-layers.js
@@ -0,0 +1,64 @@
+var t = require('tap')
+var photoshop = require('../')
+
+t.test("stream layers", function(t){
+  
+  var readStream = photoshop.createStream(function(writeStream, psd){
+    function $time(event){
+      if ($time.event) writeStream.writeln(JSON.stringify({key:'duration', event:$time.event, duration:$.hiresTimer/1000}))
+      else $.hiresTimer // reset to 0
+      $time.event = event
+    }
+    
+    $time('open')
+    
+    app.open(File(psd))
+    
+    $time('getLayerIds')
+    
+    PSFakeDOM.getLayerIds().forEach(function(id){
+      
+      $time('getMetaDataForLayerId')
+      
+      writeStream.write(
+        JSON.stringify({ key:'meta', id:id, value:
+          PSFakeDOM.getMetaDataForLayerId(
+            id)}))
+      
+      $time('getParsedMetaDataForLayerId')
+      
+      writeStream.write(
+        JSON.stringify({ key:'parsed', id:id, value:
+          PSFakeDOM.getParsedMetaDataForLayerId(
+            id)}))
+      
+      $time('layer')
+      
+      writeStream.write(
+        JSON.stringify({ key:'layer', id:id, value:
+          PSFakeDOM.executeActionGet(
+            PSFakeDOM.layerRefForId(
+              id))}))
+    })
+    
+    $time('close')
+    
+    app.activeDocument.close()
+    
+    $time()
+    
+  }, [__dirname + '/stuff/some stuff.psd'])
+  
+  readStream
+  .pipe(require('JSONStream').parse())
+  .on('data', function(object){
+    t.ok('key' in object)
+    console.dir(object)
+  })
+  // .pipe(process.stdout)
+  
+  readStream.on('end', function(){
+    t.end()
+  })
+  
+})
