5e8bffbf83d9551ae7702949ffc6de88d6a09aab
Thomas Aylott <oblivious@subtlegradient.com>
2013-04-29 13:41:26 -0700

    don't let the test run forever

--- test/test-PSDocumentHTML.js
+++ test/test-PSDocumentHTML.js
@@ -8,6 +8,8 @@ t.test("PSDocumentHTML should be a thing", function(t){
     psd: __dirname + '/stuff/some stuff.psd'
   })
   
+  readStream.on('end', t.end.bind(t))
+  
   readStream.pipe(process.stdout)
   
 })
