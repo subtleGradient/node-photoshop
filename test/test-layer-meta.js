1b295d9618be3292c6fe9b7f2fb9fc35508a458a
Thomas Aylott <oblivious@subtlegradient.com>
2013-05-08 15:44:10 -0700

    test cleanup

--- test/test-layer-meta.js
+++ test/test-layer-meta.js
@@ -1,4 +1,4 @@
-var test = require('tap').test
+var t = require('tap')
 var photoshop = require('../')
 
 function openFile_jsx(path){
@@ -7,22 +7,28 @@ function openFile_jsx(path){
   return app.activeDocument.fullName.fsName
 }
 
-test('open a file', function(t){
+t.test('open a file', function(t){
   var psd = __dirname + '/stuff/some stuff.psd'
   photoshop.invoke(openFile_jsx, [psd], function(error, path){
-    t.ok(error == null, 'no error')
+    t.notOk(error, 'no error')
+    t.is(path, psd, 'psd path')
+    
     
     photoshop.invoke('PSFakeDOM.getParsedMetaDataForLayerId', [8], function(error, parsedMetadata){
-      t.ok(error == null, 'no error')
-      t.is(typeof parsedMetadata, 'object')
-      t.ok('source' in parsedMetadata, 'parsedMetadata.source exists')
-      t.equals(parsedMetadata.source, "http://graph.facebook.com/subtlegradient/picture")
+      t.notOk(error, 'no error')
+      t.is(typeof parsedMetadata, 'object', 'parsedMetadata')
+      if (parsedMetadata){
+        t.ok('source' in parsedMetadata, 'parsedMetadata.source exists')
+        t.equals(parsedMetadata.source, "http://graph.facebook.com/subtlegradient/picture", "Layer has the correct source uri")
+      }
+    })
+    photoshop.invoke('app.activeDocument.close', function(error, result){
+      if (error) t.fail(error)
+      t.notOk(error, 'no error for closing document')
+      t.ok(result == null, "no result for closing document")
+      t.is(typeof result, 'undefined', "no result for closing document")
       t.end()
     })
     
-    photoshop.invoke('app.activeDocument.close', function(error){
-      t.ok(error == null, 'no error')
-      t.end()
-    })
   })
 })
